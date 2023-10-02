b6cf26847851b1ad7aa03b06843d74da0096a3f9
fLORIAN <71528743+OfLORIAnO@users.noreply.github.com>
2023-10-03 01:55:00 +0300

    ready app

--- src/Components/FetchTodos.tsx
+++ src/Components/FetchTodos.tsx
@@ -1,13 +1,18 @@
 import { FC } from 'react';
 import { useTodos } from '../zustand/store';
 import { Button } from '@chakra-ui/react';
+import { shallow } from 'zustand/shallow';
+
 
 const FetchTodos: FC = () => {
-    const { loading, error, fetchTodos } = useTodos((state) => ({
-        loading: state.loading,
-        error: state.error,
-        fetchTodos: state.fetchTodos,
-    }));
+    const { loading, error, fetchTodos } = useTodos(
+        (state) => ({
+            loading: state.loading,
+            error: state.error,
+            fetchTodos: state.fetchTodos,
+        }),
+        shallow
+    );
 
     return (
         <Button onClick={fetchTodos} isLoading={loading}>
